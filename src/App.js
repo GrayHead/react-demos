0cfc64f71ad5d4fb470ac02d7cbcc53d53a9171f
GrayHead <ss.zhuravlov@gmail.com>
2021-12-25 18:41:13 +0200

    demo 5 forms 2 refs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {createRef} from "react";
 
 function App() {
-    return (
-        <div>
+    let login = createRef();
+    let password = createRef();
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(login.current.value)
+        console.log(password.current.value)
+        // create obj & send to api
 
-        </div>
-    );
+    }
+
+    return (<div>
+        <form onSubmit={onSubmit}>
+            <input type="text" name={'login'} ref={login}/>
+            <input type="text" name={'password'} ref={password}/>
+            <button>log in</button>
+
+        </form>
+
+    </div>);
 }
 
 export default App;
