b082e59ba6206d44500271a90c58edd3f7e5c8f8
GrayHead <ss.zhuravlov@gmail.com>
2022-01-29 10:23:31 +0200

    init custom_hooks repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import {useFetch} from "./hooks/useFetch";
 
 function App() {
+    let {setUsers, users} = useFetch();
+
+
     return (
         <div>
+            <button onClick={() => {
+                setUsers([{name: 'xxx'}]);
+            }}>asd
+            </button>
+            {
+                users.map((value) => <div> {value.name}</div>)
+            }
 
         </div>
     );
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+
+export const useFetch = (url) => {
+
+    let [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch(url || 'https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => setUsers(value));
+
+    }, []);
+    return {users, setUsers};
+
+
+};
